a09a4e23a23858ce221e6fcf6950f0354ddd77dd
Abhijith v <abhijithababhijith@gmail.com>
2022-01-29 14:07:48 +0530

    added expense page design

--- src/pages/add-expense/index.tsx
+++ src/pages/add-expense/index.tsx
@@ -74,10 +74,10 @@ const AddExpense: NextPage = () => {
     return (
         <>
             <SideBar />
-            <div className="md:pl-72 flex flex-col flex-1  h-screen overflow-y-auto">
+            <div className="md:pl-72 flex flex-col flex-1  h-screen overflow-y-auto bg-gray-900">
                 <div className="py-6">
                     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                        <h1 className="text-2xl  text-gray-900 font-mono">Add Expense</h1>
+                        <h1 className="text-2xl  text-white font-mono">Add Expense</h1>
                     </div>
                     <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
 
