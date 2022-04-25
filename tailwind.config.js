57a64648500045fbf4e642ae74f5fc48188da689
Mohamed Hasim PK <hashim@appmaker.xyz>
2022-04-25 16:14:34 +0530

    tailwind upgrad

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,8 @@
 module.exports = {
-  purge: [
-    './src/**/*.{js,ts,jsx,tsx}',
-    // Add more here
-  ],
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
   ],
-  darkMode: false, // or 'media' or 'class'
   theme: {
     screen: {
       sm: '200px',
