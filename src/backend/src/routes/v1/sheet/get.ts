63dc9e715ae86275ebc674f266779ed6f9665bca
Abhijith v <abhijithababhijith@gmail.com>
2022-05-05 23:40:18 +0530

    added example

--- src/backend/src/routes/v1/sheet/get.ts
+++ src/backend/src/routes/v1/sheet/get.ts
@@ -1,5 +1,6 @@
 import { getAccessToken, verifyKey,verifySecret } from '../../../../helper/token';
 import { getSpreadSheetValue as getValue } from '../../../api-helper/sheet';
+
 const getSpreadSheetValue = async ({ sheetId }: { sheetId: any }, auth: { API_KEY: any, API_SECRET: any }) => {
     const tokenData: any = await verifyKey(auth.API_KEY);
     await verifySecret(auth.API_SECRET, tokenData.uid);
