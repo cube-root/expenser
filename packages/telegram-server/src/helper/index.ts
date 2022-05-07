5a567f631c21a6ea52740586bf6e61a03aeb6dc9
Abhijith v <abhijithababhijith@gmail.com>
2022-05-07 14:49:10 +0530

    eslint

--- packages/telegram-server/src/helper/index.ts
+++ packages/telegram-server/src/helper/index.ts
@@ -1,14 +1,15 @@
-const convertArgs = (data: Array<string>)=>{
-    const argsValue: any = {};
-    data.forEach((args:string)=>{
-        let key = args.split('=')[0];
-        const value = args.split('=')[1];
-        key = key.split('--')[1];
-        argsValue[key] = value;
-    })
-    return argsValue;
-}
+const convertArgs = (data: Array<string>) => {
+  const argsValue: any = {};
+  data.forEach((args:string) => {
+    let key = args.split('=')[0];
+    const value = args.split('=')[1];
+    // eslint-disable-next-line prefer-destructuring
+    key = key.split('--')[1];
+    argsValue[key] = value;
+  });
+  return argsValue;
+};
 
-export {
-    convertArgs
-}
+export default {
+  convertArgs,
+};
