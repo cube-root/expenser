f74dbad87445045fd0bb2a7cc64de137f7287d35
Abhijith v <abhijithababhijith@gmail.com>
2022-05-08 01:15:02 +0530

    delete unused file

--- packages/next/src/backend/helper/sheets/template/index.ts
+++
@@ -1,9 +0,0 @@
-import { columnTemplate, mapNumberToColumnLabel } from './template';
-import helper from './helper';
-const template = {
-  columnTemplate,
-  mapNumberToColumnLabel,
-  helper,
-};
-
-export default template;
