c5b810a06bb3dc008fb357be9cba138da2b92158
Abhijith v <abhijithababhijith@gmail.com>
2022-05-15 23:19:35 +0530

    wip:telegram

--- packages/telegram-server/src/index.ts
+++ packages/telegram-server/src/index.ts
@@ -1,17 +1,11 @@
-/* eslint-disable import/extensions */
 /* eslint-disable import/no-unresolved */
+/* eslint-disable import/extensions */
 /* eslint-disable consistent-return */
 import 'dotenv/config';
-import { Telegraf } from 'telegraf';
-import axios from 'axios';
-import helper from './helper';
-
-const { convertArgs } = helper;
-
-const url = process.env.URL || 'http://localhost:3000';
+import TelegramBot from 'node-telegram-bot-api';
+import api from './helper/api';
 
 const botToken = process.env.BOT_TOKEN || '';
-const bot = new Telegraf(botToken);
 const message = `
 Expense \n\n\n
 1. Configure your API key and secret eg: /configure ajknsd ajsdiw  \n
@@ -20,77 +14,94 @@ command: /configure API_KEY API_SECRET
 2. Add Expense \n
 command: /add --amount=250 --type=food --currency=$ --remark=test\n
 `;
-bot.start((ctx:any) => {
-  ctx.reply(message, {
-    parse_mode: 'HTML',
-  });
-});
 
-bot.command('/configure', async (ctx: any) => {
-  const x = ctx.message;
-  let keys = x.text.split('/configure');
-  keys = keys.filter((key: any) => key !== '');
-  const API_SECRET = keys[0];
-  ctx.reply('Please wait while configuring');
-  try {
-    if (!API_SECRET) {
-      ctx.reply('Please enter API_SECRET');
-    } else {
-      await axios.post(`${url}/api/v1/integrations/telegram/configure`, {
-        API_SECRET,
-        CHAT_ID: ctx.chat.id,
-      });
-      ctx.reply('Configured Successfully');
-    }
-  } catch (error) {
-    ctx.reply('Something went wrong');
-  }
-});
+class Bot {
+  bot: TelegramBot;
 
-bot.command('/add', async (ctx: any) => {
-  const x = ctx.message;
-  let keys = x.text.split('/add').filter((y: any) => y !== '');
-  keys = keys[0].split(' ').filter((y: any) => y !== '');
-  const data: {
-        amount?: number,
-        remark?: string,
-        type?: string,
-        currency?: string
-    } = convertArgs(keys);
-  const {
-    amount,
-    remark,
-    type,
-    currency,
-  } = data;
-  if (!amount) {
-    return ctx.reply('Please enter amount');
+  constructor(token: string) {
+    this.bot = new TelegramBot(token, { polling: true });
   }
 
-  ctx.reply('Please wait while adding expense to sheet');
-  try {
-    if (!amount || !type) {
-      ctx.reply('Please enter all the fields');
-    } else {
-      await axios.post(`${url}/api/v1/integrations/telegram/add-expense`, {
-        data: {
-          amount,
-          remark,
-          type,
-          symbol: currency,
-        },
-      }, {
-        headers: {
-          chat_id: ctx.chat.id,
-          'Content-Type': 'application/json',
+  start() {
+    console.log('bot started....');
+    this.bot.on('new_chat_members', (msg:any) => {
+      this.bot.sendMessage(msg.chat.id, message);
+    });
+    this.bot.onText(/\/help/, (msg: any) => {
+      const chatId = msg.chat.id;
+      this.bot.sendMessage(chatId, message);
+    });
+  }
+
+  configure() {
+    this.bot.onText(/\/configure/, async (msg: any) => {
+      const apiPrompt = await this.bot.sendMessage(msg.chat.id, 'Hi, Please enter your API_SECRET HERE', {
+        reply_markup: {
+          force_reply: true,
         },
       });
-      ctx.reply('Expense added to sheet');
-    }
-  } catch (error) {
-    ctx.reply('Something went wrong');
+      const chatId = msg.chat.id;
+      this.bot.onReplyToMessage(chatId, apiPrompt.message_id, async (secret: any) => {
+        const apiSecret = secret.text;
+        if (!apiSecret || apiSecret.length === 0) {
+          return this.bot.sendMessage(chatId, 'Please enter API_SECRET');
+        }
+        try {
+          await this.bot.sendMessage(chatId, 'Configuring... Please wait');
+          await api.configure(apiSecret, chatId);
+          await this.bot.sendMessage(chatId, 'Configured successfully');
+        } catch (error: any) {
+          await this.bot.sendMessage(chatId, 'Something went wrong');
+          if (error.response) { await this.bot.sendMessage(chatId, `${error.message}`); }
+        }
+      });
+    });
   }
-});
 
-bot.launch();
-console.log('bot started...!');
+  async replyBot(chatId: number, question: string, callback?: any) {
+    const prompt = await this.bot.sendMessage(chatId, question, {
+      reply_markup: {
+        force_reply: true,
+      },
+    });
+    if (callback) {
+      this.bot.onReplyToMessage(chatId, prompt.message_id, ((answer: any) => {
+        if (callback) {
+          callback(answer);
+        }
+      }));
+    } else {
+      return new Promise((resolve: any) => {
+        this.bot.onReplyToMessage(chatId, prompt.message_id, ((answer: any) => {
+          resolve(answer);
+        }));
+      });
+    }
+  }
+
+  add() {
+    this.bot.onText(/\/add/, async (msg: any) => {
+      const amount: any = await this.replyBot(msg.chat.id, 'Enter Amount');
+      const remark: any = await this.replyBot(msg.chat.id, 'Enter Remark');
+      const type: any = await this.replyBot(msg.chat.id, 'Enter type (eg:food, travel, etc)');
+      await this.bot.sendMessage(msg.chat.id, 'Adding to sheet. Please wait...');
+      try {
+        await api.add({
+          amount: amount.text,
+          remark: remark.text,
+          type: type.text,
+          chatId: msg.chat.id,
+        });
+        await this.bot.sendMessage(msg.chat.id, 'Added successfully');
+      } catch (error: any) {
+        await this.bot.sendMessage(msg.chat.id, 'Something went wrong');
+        if (error.response) { await this.bot.sendMessage(msg.chat.id, `${error.message}`); }
+      }
+    });
+  }
+}
+
+const bot = new Bot(botToken);
+bot.start();
+bot.configure();
+bot.add();
