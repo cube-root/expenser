1946c3cc2b21eca17ba8e340ae419af286f5ab10
Abhijith v <abhijithababhijith@gmail.com>
2022-05-15 14:29:31 +0530

    fix-home

--- packages/next/src/pages/home/index.tsx
+++ packages/next/src/pages/home/index.tsx
@@ -76,7 +76,7 @@ const Home = () => {
           </div>
         )}
         {!isLoading && (
-          <div className="md:pl-72 flex flex-col flex-1  h-screen overflow-y-auto">
+          <div className="flex flex-col flex-1  h-screen overflow-y-auto">
             <div className="py-6">
               <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <h1 className="text-2xl font-mono text-black">Home</h1>
