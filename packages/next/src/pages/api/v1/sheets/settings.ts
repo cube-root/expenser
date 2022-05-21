bfbf780ac8d151766f9054452599ee654310290c
Abhijith v <abhijithababhijith@gmail.com>
2022-05-21 12:26:24 +0530

    sheet settings api

--- packages/next/src/pages/api/v1/sheets/settings.ts
+++ packages/next/src/pages/api/v1/sheets/settings.ts
@@ -1,11 +1,27 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-const api = (req:NextApiRequest,res:NextApiResponse)=>{
-    if(req.method === 'GET'){
-        return res.status(200).json({
-            message: 'Get api success'
-        })
+import {
+    settings
+} from '../../../../backend'
+const api = async (req: NextApiRequest, res: NextApiResponse) => {
+    if (req.method === 'GET') {
+        try {
+            const {
+                'x-api_key': API_KEY,
+                'x-api_secret' :API_SECRET
+            } = req.headers;
+            if (!API_KEY || !API_SECRET) {
+                throw new Error('Keys missing');
+            }
+            const value = await settings.get({
+                API_KEY: API_KEY,
+                API_SECRET: API_SECRET
+            })
+            return res.status(200).json(value)
+        } catch (error: any) {
+            return res.status(500).json({
+                message: error.message || 'Get api failed'
+            })
+        }
     }
 }
-
-
 export default api;
