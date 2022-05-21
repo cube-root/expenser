ba29eb3581b6fbd1e59c142be26a2f3afa406adb
Abhijith v <abhijithababhijith@gmail.com>
2022-05-21 13:44:22 +0530

    added settins api

--- packages/next/src/pages/api/v1/settings/general.ts
+++ packages/next/src/pages/api/v1/settings/general.ts
@@ -1,15 +1,16 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import {settings} from '../../../../backend';
+import { settings } from '../../../../backend';
+
 const api = async (req: NextApiRequest, res: NextApiResponse) => {
+    const {
+        'x-api_key': API_KEY,
+        'x-api_secret': API_SECRET
+    } = req.headers;
+    if (!API_KEY || !API_SECRET) {
+        return res.status(401).send("Unauthenticated");
+    }
     if (req.method === 'GET') {
         try {
-            const {
-                'x-api_key': API_KEY,
-                'x-api_secret' :API_SECRET
-            } = req.headers;
-            if (!API_KEY || !API_SECRET) {
-                throw new Error('Keys missing');
-            }
             const response = await settings.getGeneralSettings({
                 API_KEY: API_KEY,
                 API_SECRET: API_SECRET
