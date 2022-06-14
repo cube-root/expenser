0d2826e2ccbf3d6962f902716d099959fff74190
Abhijith v <abhijithababhijith@gmail.com>
2022-06-14 23:51:56 +0530

    add expense fix

--- packages/next/src/pages/add-expense/index.tsx
+++ packages/next/src/pages/add-expense/index.tsx
@@ -5,19 +5,22 @@ import axios from 'axios';
 import { toast } from 'react-toastify';
 import Forms from '../../components/forms';
 import SideBar from '../../components/sidebar';
-import getStorageData from '../../hooks/get-data';
-import helper from '../../helper';
+import withUser from '../../wrapper/check-user';
+import useUser from '../../hooks/user';
+import useSheet from '../../hooks/sheet';
+import * as yup from 'yup';
 import { ChevronDownIcon } from '@heroicons/react/solid';
 
 const AddExpense = () => {
   const router = useRouter();
   const [isLoading, setLoading] = useState(false);
-  const { data: storage } = getStorageData(helper.getFirebaseConfig());
+  const [user] = useUser();
+  const [sheet] = useSheet();
   const amount = useRef<any>(0);
   const remark = useRef<string>('');
   const type = useRef<string>('food');
   // const currency = useRef<string>('$');
-  const fetchData = async ({
+  const appendData = async ({
     sheetId,
     inputData,
   }: {
@@ -34,8 +37,8 @@ const AddExpense = () => {
         {
           headers: {
             'Content-Type': 'application/json',
-            API_KEY: storage.API_KEY,
-            API_SECRET: storage.API_SECRET,
+            API_KEY: user.API_KEY,
+            API_SECRET: user.API_SECRET,
           },
         },
       );
@@ -48,26 +51,39 @@ const AddExpense = () => {
   };
   const formSubmit = async (event: any) => {
     event.preventDefault();
-    if (global) {
+    const schema = yup.object().shape({
+      amount: yup.number().required(),
+      remark: yup.string(),
+      type: yup.string().required(),
+    })
+    try {
+      const validateData = await schema.validate({
+        amount: amount.current,
+        remark: remark.current,
+        type: type.current,
+      })
       setLoading(true);
-      const sheetId =
-        storage && storage.sheet ? storage.sheet.spreadSheetId : undefined;
+      const sheetId = sheet.sheetId
       if (sheetId === null || !sheetId) {
         router.push('/home');
         return false;
       }
-      //symbol: currency.current,
-      await fetchData({
+      await appendData({
         sheetId,
-        inputData: {
-          amount: amount.current,
-          remark: remark.current,
-          type: type.current,
-        },
+        inputData: validateData,
       });
-      setLoading(false);
+      event.target.reset()
+    } catch (error: any) {
+      if (error.errors) {
+        error.errors && error.errors.forEach((err: any) => {
+          toast.error(err);
+        })
+      } else {
+        toast.error(error.message || 'Failed to add expense');
+      }
     }
-    event;
+    setLoading(false);
+
   };
 
   return (
@@ -155,32 +171,6 @@ const AddExpense = () => {
               />
             </div>
           </div>
-
-          {/* <div className="sm:col-span-4">
-                  <label htmlFor="email" className="block text-sm font-medium text-slate-700 dark:text-slate-200">
-                    Currency
-                  </label>
-                  <div className="mt-1">
-                    <Forms.CurrencyFormField
-                      required
-                      name="currency"
-                      onChange={(event: any) => {
-                        currency.current = event.target.value;
-                      }}
-                      className="  block appearance-none w-full bg-white border border-gray-500 text-vlack py-3 px-4 pr-8 rounded-md leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                      id="grid-state"
-                    />
-                    <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                      <svg
-                        className="fill-current h-4 w-4"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 20 20"
-                      >
-                        <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                      </svg>
-                    </div>
-                  </div>
-                </div> */}
         </div>
 
         <div className="pt-6">
@@ -199,4 +189,4 @@ const AddExpense = () => {
   );
 };
 
-export default AddExpense;
+export default withUser(AddExpense);
